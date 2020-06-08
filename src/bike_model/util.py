05f27963a7c4133cdeac32582e544c18b58e551a
patriciallull <patricia.llull@gmail.com>
2020-06-08 22:40:04 +0200

    utils functions

--- src/bike_model/util.py
+++ src/bike_model/util.py
@@ -37,8 +37,7 @@ def get_season(date):
         return 2
     elif SUMMER_SOLSTICE <= date < FALL_EQUINOX:
         return 3
-    elif FALL_EQUINOX y<= date < WINTER_SOLSTICE:
+    elif FALL_EQUINOX <= date < WINTER_SOLSTICE:
         return 4
     else:
         return 1
-        
