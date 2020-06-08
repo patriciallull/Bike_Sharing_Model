4850d11778dc3ff0e14fc3c67ba59e3d4c33cbe5
patriciallull <patricia.llull@gmail.com>
2020-06-08 21:49:21 +0200

    utils functions

---
+++ src/bike_model/util.py
@@ -0,0 +1,44 @@
+import datetime as dt
+import os
+import pandas as pd
+
+WINTER_SOLSTICE = dt.datetime(2000,12,21)
+SPRING_EQUINOX = dt.datetime(2000,3,21)
+SUMMER_SOLSTICE = dt.datetime(2000,6,21)
+FALL_EQUINOX = dt.datetime(2000,9,21)
+
+def get_data_directory():
+    """
+    Retrieve Data from the directory where it is located
+    """
+    lib_dir = os.path.dirname(__file__)
+    data_dir = os.path.join(lib_dir, "data")
+    return data_dir
+
+
+def get_model_path(model_dir = None):#set default to none in case it does not exist
+    if model_dir is None:
+        model_dir = os.path.dirname(__file__)
+
+    model_path = os.path.join(model_dir, "model.pkl")
+    return model_path
+
+def read_data(hour_path = None):
+    if hour_path is None:
+        hour_path = os.path.join(get_data_directory(), "hour.csv")
+
+    hour = pd.read_csv(hour_path, index_col = "instant", parse_dates = True)
+    return hour
+
+def get_season(date):
+    date = date.replace(year=2000) # we will use the equinox/solstices
+
+    if SPRING_EQUINOX <= date < SUMMER_SOLSTICE:
+        return 2
+    elif SUMMER_SOLSTICE <= date < FALL_EQUINOX:
+        return 3
+    elif FALL_EQUINOX y<= date < WINTER_SOLSTICE:
+        return 4
+    else:
+        return 1
+        
